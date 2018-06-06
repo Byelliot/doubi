e4b0976dc9c3ea311fe4217b36d0ee2c570e51c7
Miku <xcxnig@gmail.com>
2018-06-07 07:32:25 +0800

    Update status.sh

--- status.sh
+++ status.sh
@@ -240,8 +240,8 @@ Set_password(){
 	else
 		echo -e "请输入 ServerStatus 服务端中对应配置的密码[password]（字母/数字）"
 	fi
-	stty erase '^H' && read -p "(默认: doub.io):" password_s
-	[[ -z "$password_s" ]] && password_s="doub.io"
+	stty erase '^H' && read -p "(默认: qazCloud):" password_s
+	[[ -z "$password_s" ]] && password_s="qazCloud"
 	echo && echo "	================================================"
 	echo -e "	密码[password]: ${Red_background_prefix} ${password_s} ${Font_color_suffix}"
 	echo "	================================================" && echo
